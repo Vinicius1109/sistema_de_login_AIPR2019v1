34ba30a9234511e64cf03fa371e01bfa21379ab3
Vinicius De Souza <vinicius_souza9@estudante.sc.senai.br>
2019-09-05 15:37:27 -0300

    Criação do profile.php
    Recepção dos dados do front-end no back-end e redirecionamento para o profile.php

--- index.php
+++ index.php
@@ -150,6 +150,10 @@
                         success: function(resposta) {
                             $('#alerta').show();
                             $('#resultado').html(resposta);
+                            if(resposta == "ok"){
+                                
+                                window.location = "profile.php";
+                            }
                         }
                     });
                 }
